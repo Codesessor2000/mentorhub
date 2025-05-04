262182d49f6a476e414ec1424142a3799c7a5a71
Chaitanya Malegaonkar <chaitanya.malegaonkar@gmail.com>
2025-05-04 15:29:52 +0530

    port change

--- backend/server.js
+++ backend/server.js
@@ -41,5 +41,5 @@ app.get(/^\/(?!api).*/, (req, res) => {
   );
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
