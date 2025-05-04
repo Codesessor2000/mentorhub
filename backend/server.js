42da2713a3dc3265eaa7882d1a3dc3302b81776d
Chaitanya Malegaonkar <chaitanya.malegaonkar@gmail.com>
2025-05-04 14:05:38 +0530

    frontend serving

--- backend/server.js
+++ backend/server.js
@@ -31,7 +31,9 @@ app.use('/api/analytics', analyticsRoutes);
 
 // Handle requests to other routes (frontend routes)
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'frontend', 'dist', 'index.html'));
+  res.sendFile(
+    path.join(__dirname, 'frontend', 'dist', 'frontend', 'index.html'),
+  );
 });
 
 const PORT = process.env.PORT || 3000;
