cb49bd023bb0610beae0a81257d1f202455a0634
Chaitanya Malegaonkar <chaitanya.malegaonkar@gmail.com>
2025-05-04 14:43:46 +0530

    path import

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from 'path';
 import authRoutes from './routes/authRoutes.js';
 import profileRoutes from "./routes/profileRoutes.js";
 import availabilityRoutes from "./routes/availabilityRoutes.js";
